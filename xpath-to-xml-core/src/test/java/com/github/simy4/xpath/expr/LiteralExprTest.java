cc1aef6d69335fb5b40630793469f723d252ebcf
Alex Simkin <asimkin@atlassian.com>
2017-06-19 12:52:10 +1000

    literal test fix

--- xpath-to-xml-core/src/test/java/com/github/simy4/xpath/expr/LiteralExprTest.java
+++ xpath-to-xml-core/src/test/java/com/github/simy4/xpath/expr/LiteralExprTest.java
@@ -11,17 +11,17 @@ import org.junit.Rule;
 import org.junit.Test;
 import org.junit.experimental.theories.DataPoints;
 import org.junit.experimental.theories.FromDataPoints;
+import org.junit.experimental.theories.Theories;
 import org.junit.experimental.theories.Theory;
 import org.junit.runner.RunWith;
 import org.mockito.Mock;
 import org.mockito.junit.MockitoJUnit;
-import org.mockito.junit.MockitoJUnitRunner;
 import org.mockito.junit.MockitoRule;
 
 import static com.github.simy4.xpath.utils.StringNode.node;
 import static org.assertj.core.api.Assertions.assertThat;
 
-@RunWith(MockitoJUnitRunner.class)
+@RunWith(Theories.class)
 public class LiteralExprTest {
 
     @DataPoints("parent nodes") public static View[] parentNodes = {
