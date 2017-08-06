6cfb0543f5918699adc3c47fa63f0a6f41589f6c
Alex Simkin <asimkin@atlassian.com>
2017-08-06 19:00:33 +1000

    Eagerly consume resolved nodes on Put

--- xpath-to-xml-core/src/main/java/com/github/simy4/xpath/effects/PutEffect.java
+++ xpath-to-xml-core/src/main/java/com/github/simy4/xpath/effects/PutEffect.java
@@ -4,7 +4,10 @@ import com.github.simy4.xpath.XmlBuilderException;
 import com.github.simy4.xpath.expr.Expr;
 import com.github.simy4.xpath.navigator.Navigator;
 import com.github.simy4.xpath.navigator.Node;
+import com.github.simy4.xpath.view.AbstractViewVisitor;
+import com.github.simy4.xpath.view.IterableNodeView;
 import com.github.simy4.xpath.view.NodeView;
+import com.github.simy4.xpath.view.View;
 import com.github.simy4.xpath.view.ViewContext;
 
 public class PutEffect implements Effect {
@@ -19,7 +22,23 @@ public class PutEffect implements Effect {
     public <N extends Node> void perform(Navigator<N> navigator) throws XmlBuilderException {
         final NodeView<N> xml = new NodeView<N>(navigator.xml());
         final ViewContext<N> context = new ViewContext<N>(navigator, xml, true);
-        expr.resolve(context);
+        expr.resolve(context).visit(new EagerVisitor<N>());
+    }
+
+    private static final class EagerVisitor<N extends Node> extends AbstractViewVisitor<N, Void> {
+
+        @Override
+        @SuppressWarnings("StatementWithEmptyBody")
+        public Void visit(IterableNodeView<N> nodeSet) throws XmlBuilderException {
+            for (NodeView<N> ignored : nodeSet) { } // eagerly consume resolved iterable
+            return null;
+        }
+
+        @Override
+        protected Void returnDefault(View<N> view) {
+            return null;
+        }
+
     }
 
 }
