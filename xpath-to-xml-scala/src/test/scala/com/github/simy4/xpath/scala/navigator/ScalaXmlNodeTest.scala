2a7bf44fb40508a7ab12cf7ee3c6767087105216
Alex Simkin <asimkin@atlassian.com>
2018-10-24 17:12:10 +1100

    More tests.

--- xpath-to-xml-scala/src/test/scala/com/github/simy4/xpath/scala/navigator/ScalaXmlNodeTest.scala
+++ xpath-to-xml-scala/src/test/scala/com/github/simy4/xpath/scala/navigator/ScalaXmlNodeTest.scala
@@ -5,17 +5,63 @@ import org.junit.jupiter.api.Test
 
 class ScalaXmlNodeTest {
   import Assertions._
+  import scala.collection.JavaConverters._
 
-  private val xml = <root/>
+  private val xml = <root attr="value"/>
+  private val root = new Root(xml)
 
   @Test
   def shouldThrowWhenRootNameAccessed(): Unit = {
-    assertThatThrownBy(() => new Root(xml).getName).isInstanceOf(classOf[UnsupportedOperationException])
+    assertThatThrownBy(() => root.getName) isInstanceOf classOf[UnsupportedOperationException]
   }
 
   @Test
   def shouldThrowWhenRootTextAccessed(): Unit = {
-    assertThatThrownBy(() => new Root(xml).getText).isInstanceOf(classOf[UnsupportedOperationException])
+    assertThatThrownBy(() => root.getText) isInstanceOf classOf[UnsupportedOperationException]
+  }
+
+  @Test
+  def shouldReturnRootElementWhenRootElementsAccessed(): Unit = {
+    assertThat(root.elements.asJava) containsExactly Element(xml, 0, root)
+  }
+
+  @Test
+  def shouldReturnNilWhenRootAttributesAccessed(): Unit = {
+    assertThat(root.attributes.asJava).isEmpty()
+  }
+
+  @Test
+  def shouldReturnNullWhenRootParentAccessed(): Unit = {
+    assertThat(root.parent).isNull()
+  }
+
+  @Test
+  def shouldReturnParentWhenElementParentAccessed(): Unit = {
+    val element = root.elements.head
+
+    assertThat(element.parent) isEqualTo root
+  }
+
+  @Test
+  def shouldReturnParentWhenAttributeParentAccessed(): Unit = {
+    val parent = root.elements.head
+    val attribute = parent.attributes.head
+
+    assertThat(attribute.parent) isEqualTo parent
+  }
+
+  @Test
+  def shouldReturnNilWhenAttributeElementsAccessed(): Unit = {
+    val attribute = root.elements.head.attributes.head
+
+    assertThat(attribute.elements.asJava).isEmpty()
+  }
+
+  @Test
+  def shouldReturnNilWhenAttributeAttributesAccessed(): Unit = {
+    val attribute = root.elements.head.attributes.head
+
+    assertThat(attribute.attributes.asJava).isEmpty()
   }
 
 }
