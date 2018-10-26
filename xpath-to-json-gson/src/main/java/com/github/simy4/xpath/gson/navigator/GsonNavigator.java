b849334e432af2e2f2571c02f5b3f52c347b5873
Alex Simkin <asimkin@atlassian.com>
2018-10-26 14:06:36 +1100

    Fixing tests.
    (cherry picked from commit 15627c59fec8cc64c372aa0b017472abce882266)

--- xpath-to-json-gson/src/main/java/com/github/simy4/xpath/gson/navigator/GsonNavigator.java
+++ xpath-to-json-gson/src/main/java/com/github/simy4/xpath/gson/navigator/GsonNavigator.java
@@ -97,9 +97,10 @@ public class GsonNavigator implements Navigator<GsonNode> {
                 if (parentParentElement.isJsonArray()) {
                     final JsonArray jsonArray = parentParentElement.getAsJsonArray();
                     copyNode = prependToArray(parentParent, parentElement, jsonArray);
-                    parent.setParent(new GsonByIndexNode(jsonArray, copyNode.getIndex() + 1, node.getParent()));
+                    parent.setParent(new GsonByIndexNode(jsonArray, copyNode.getIndex() + 1, parentParent));
                 } else {
                     copyNode = prependToNewArray(parent, parentElement);
+                    node.setParent(new GsonByIndexNode(parent.get().getAsJsonArray(), copyNode.getIndex() + 1, parent));
                 }
             } else {
                 copyNode = prependToNewArray(parent, parentElement);
@@ -109,7 +110,7 @@ public class GsonNavigator implements Navigator<GsonNode> {
         } else if (parentElement.isJsonArray()) {
             final JsonArray jsonArray = parentElement.getAsJsonArray();
             final GsonByIndexNode copyNode = prependToArray(parent, elementToCopy, jsonArray);
-            node.setParent(new GsonByIndexNode(jsonArray, copyNode.getIndex() + 1, node.getParent()));
+            node.setParent(new GsonByIndexNode(jsonArray, copyNode.getIndex() + 1, parent));
             elementNode = copyNode;
         } else {
             throw new XmlBuilderException("Unable to prepend copy to primitive node: " + parentElement);
