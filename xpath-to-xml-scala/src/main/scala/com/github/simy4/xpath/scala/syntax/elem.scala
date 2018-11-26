f5c054edf6be2cf701728d6c1793ec0e6eb2da46
SimY4 <alex.simkin@outlook.com>
2018-11-26 11:51:55 +1100

    Scala: compile against 2.13

--- xpath-to-xml-scala/src/main/scala/com/github/simy4/xpath/scala/syntax/elem.scala
+++ xpath-to-xml-scala/src/main/scala/com/github/simy4/xpath/scala/syntax/elem.scala
@@ -21,14 +21,14 @@ final class XmlElemOps(private val elem: Elem) extends AnyVal {
       .put(xpath, value)
       .build(elem)
   def putAll(xpaths: Iterable[String])(implicit namespaceContext: NamespaceContext = null): Elem =
-    (new XmlBuilder(namespaceContext) /: xpaths) { _.put(_) } build elem
+    xpaths.foldLeft(new XmlBuilder(namespaceContext)) { _.put(_) } build elem
   def putAllValues(xpathToValueMap: Map[String, AnyRef])(implicit namespaceContext: NamespaceContext = null): Elem =
-    (new XmlBuilder(namespaceContext) /: xpathToValueMap) {
+    xpathToValueMap.foldLeft(new XmlBuilder(namespaceContext)) {
       (builder, entry) => builder.put(entry._1, entry._2)
     } build elem
   def remove(xpath: String)(implicit namespaceContext: NamespaceContext = null): Elem = new XmlBuilder(namespaceContext)
     .remove(xpath)
     .build(elem)
   def removeAll(xpaths: Iterable[String])(implicit namespaceContext: NamespaceContext = null): Elem =
-    (new XmlBuilder(namespaceContext) /: xpaths) { _.remove(_) } build elem
+    xpaths.foldLeft(new XmlBuilder(namespaceContext)) { _.remove(_) } build elem
 }
