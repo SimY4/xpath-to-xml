cf6bdbb393e4aaaa5e9001afadf58393a7e94142
SimY4 <alex.simkin@outlook.com>
2019-02-19 20:11:24 +1100

    Simplify DOM node test.

--- xpath-to-xml-dom/src/test/java/com/github/simy4/xpath/dom/navigator/DomNodeTest.java
+++ xpath-to-xml-dom/src/test/java/com/github/simy4/xpath/dom/navigator/DomNodeTest.java
@@ -1,41 +1,29 @@
 package com.github.simy4.xpath.dom.navigator;
 
+import com.github.simy4.xpath.helpers.SerializationHelper;
 import com.github.simy4.xpath.navigator.Node;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
-import org.mockito.junit.jupiter.MockitoSettings;
-import org.mockito.quality.Strictness;
-import org.w3c.dom.Document;
 
 import javax.xml.XMLConstants;
 import javax.xml.namespace.QName;
-import javax.xml.parsers.DocumentBuilderFactory;
-import javax.xml.parsers.ParserConfigurationException;
-
-import java.io.ByteArrayInputStream;
-import java.io.ByteArrayOutputStream;
 import java.io.IOException;
-import java.io.ObjectInputStream;
-import java.io.ObjectOutputStream;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.mockito.Mockito.when;
 
 @ExtendWith(MockitoExtension.class)
-@MockitoSettings(strictness = Strictness.LENIENT)
 class DomNodeTest {
 
-    @Mock private org.w3c.dom.Node node;
+    @Mock(serializable = true) private org.w3c.dom.Node node;
 
     private Node nodeView;
 
     @BeforeEach
     void setUp() {
-        when(node.getTextContent()).thenReturn("text");
-
         nodeView = new DomNode(node);
     }
 
@@ -59,24 +47,21 @@ class DomNodeTest {
 
     @Test
     void shouldReturnNodeTextContent() {
+        when(node.getTextContent()).thenReturn("text");
+
         assertThat(nodeView.getText()).isEqualTo("text");
     }
 
     @Test
-    void shouldSerializeAndDeserialize() throws IOException, ClassNotFoundException, ParserConfigurationException {
+    void shouldSerializeAndDeserialize() throws IOException, ClassNotFoundException {
         // given
-        DocumentBuilderFactory documentBuilderFactory = DocumentBuilderFactory.newInstance();
-        Document document = documentBuilderFactory.newDocumentBuilder().newDocument();
-        nodeView = new DomNode(document);
+        when(node.getNodeName()).thenReturn("node");
 
         // when
-        ByteArrayOutputStream out = new ByteArrayOutputStream();
-        new ObjectOutputStream(out).writeObject(nodeView);
-        ByteArrayInputStream in = new ByteArrayInputStream(out.toByteArray());
-        Node deserialized = (Node) new ObjectInputStream(in).readObject();
+        Node deserializedNode = SerializationHelper.serializeAndDeserializeBack(nodeView);
 
         // then
-        assertThat(deserialized).hasToString(document.toString());
+        assertThat(deserializedNode.getName()).isEqualTo(nodeView.getName());
     }
 
 }
