12309cc4d43674c12603da877c0eefdd52840cf5
SimY4 <alex.simkin@outlook.com>
2019-03-18 13:25:05 +1100

    Core: Stop exposing util.

--- xpath-to-xml-core/src/main/java/module-info.java
+++ xpath-to-xml-core/src/main/java/module-info.java
@@ -4,7 +4,6 @@ module com.github.simych.xpath.core {
     exports com.github.simy4.xpath;
     exports com.github.simy4.xpath.navigator;
     exports com.github.simy4.xpath.spi;
-    exports com.github.simy4.xpath.util;
 
     uses com.github.simy4.xpath.spi.NavigatorSpi;
 }
