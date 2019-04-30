ff8cb298150908175358582f3075455a668c7edb
SimY4 <alex.simkin@outlook.com>
2019-04-30 12:10:26 +1000

    modularize Dom4J.

---
+++ xpath-to-xml-dom4j/src/main/java/module-info.java
@@ -0,0 +1,6 @@
+module com.github.simych.xpath.dom4j {
+    requires transitive com.github.simych.xpath.core;
+    requires dom4j;
+
+    provides com.github.simy4.xpath.spi.NavigatorSpi with com.github.simy4.xpath.dom4j.spi.Dom4jNavigatorSpi;
+}
