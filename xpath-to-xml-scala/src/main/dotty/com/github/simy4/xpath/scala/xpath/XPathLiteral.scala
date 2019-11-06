0811d9c490a58bd1c6d214f29ffe4b327cc87f6e
SimY4 <alex.simkin@outlook.com>
2019-11-06 17:25:40 +1100

    Make macros work on dotty.

---
+++ xpath-to-xml-scala/src/main/dotty/com/github/simy4/xpath/scala/xpath/XPathLiteral.scala
@@ -0,0 +1,29 @@
+package com.github.simy4.xpath.scala.xpath
+
+import com.github.simy4.xpath.expr.{ Expr => JExpr }
+import com.github.simy4.xpath.parser.XPathParser
+import javax.xml.xpath.XPathExpressionException
+
+import scala.quoted._
+
+final class XPathLiteral(val sc: StringContext) extends AnyVal
+  inline def xpath(args: Any*): JExpr = ${XPathLiteral.xpathImpl('sc)}
+
+object XPathLiteral
+  def xpathImpl(sc: Expr[StringContext])(given qctx: QuoteContext): Expr[JExpr] =
+    import qctx.tasty.{_, given}
+    sc.unseal match
+      case Apply(_, List(Apply(_, List(lit @ Literal(Constant(str: String)))))) =>
+        try
+          val _ = new XPathParser(null).parse(str)
+          '{new XPathParser(null).parse(${lit.seal.cast[String]})}
+        catch
+          case xpee: XPathExpressionException =>
+            qctx.error(s"Illegal XPath expression: ${xpee.getMessage}")
+            '{null}
+      case _ =>
+        qctx.error("xpath can only be used on string literals")
+        '{null}
+
+trait ToXPathLiteral
+  implicit def toXPathLiteral(sc: StringContext): XPathLiteral = new XPathLiteral(sc)
