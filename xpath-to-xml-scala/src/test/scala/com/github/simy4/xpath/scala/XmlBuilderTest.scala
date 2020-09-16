1937cfaf876623601a25c661b18a947f7b3b5d75
SimY4 <alex.simkin@outlook.com>
2020-09-16 15:32:09 +1000

    scalafmt 2.7.1

--- xpath-to-xml-scala/src/test/scala/com/github/simy4/xpath/scala/XmlBuilderTest.scala
+++ xpath-to-xml-scala/src/test/scala/com/github/simy4/xpath/scala/XmlBuilderTest.scala
@@ -17,7 +17,6 @@ import org.xml.sax.InputSource
 import collection.{ mutable, Map }
 import xml.{ Elem, NamespaceBinding, Node, Null, PrettyPrinter, TopScope, XML }
 
-//noinspection ConvertExpressionToSAM
 class DataProvider extends ArgumentsProvider {
 
   private val namespaceContext = new SimpleNamespaceContext
@@ -25,24 +24,25 @@ class DataProvider extends ArgumentsProvider {
 
   override def provideArguments(context: ExtensionContext): Stream[_ <: Arguments] =
     java.util.Arrays.stream(
-      Array(
-        arguments(new FixtureAccessor("simple"), null, <breakfast_menu/>),
-        arguments(new FixtureAccessor("simple"), namespaceContext, <breakfast_menu/>),
-        arguments(
+      Array[Arguments](
+        (new FixtureAccessor("simple"), null, <breakfast_menu/>),
+        (new FixtureAccessor("simple"), namespaceContext, <breakfast_menu/>),
+        (
           new FixtureAccessor("ns-simple"),
           namespaceContext,
           Elem("my", "breakfast_menu", Null, namespaceBinding, minimizeEmpty = true)
         ),
-        arguments(new FixtureAccessor("attr"), null, <breakfast_menu/>),
-        arguments(new FixtureAccessor("attr"), namespaceContext, <breakfast_menu/>),
-        arguments(new FixtureAccessor("special"), null, <records/>),
-        arguments(new FixtureAccessor("special"), namespaceContext, <records/>)
+        (new FixtureAccessor("attr"), null, <breakfast_menu/>),
+        (new FixtureAccessor("attr"), namespaceContext, <breakfast_menu/>),
+        (new FixtureAccessor("special"), null, <records/>),
+        (new FixtureAccessor("special"), namespaceContext, <records/>)
       )
     )
 
-  private def arguments(args: AnyRef*): Arguments =
+  implicit private def arguments(p: Product): Arguments =
     new Arguments {
-      override def get(): Array[AnyRef] = args.toArray
+      @SuppressWarnings(Array("org.wartremover.warts.Any", "org.wartremover.warts.AsInstanceOf"))
+      override def get(): Array[AnyRef] = p.productIterator.toArray.asInstanceOf[Array[AnyRef]]
     }
 }
 
@@ -102,12 +102,11 @@ class XmlBuilderTest {
       .unsafeGet
     val builtDocumentString = xmlToString(builtDocument)
 
-    xmlProperties.foreach {
-      case (xpath, value) =>
-        val documentSource: InputSource = builtDocumentString
-        assertThat(xpath.evaluate(documentSource, XPathConstants.STRING))
-          .as("Should evaluate XPath %s to %s", xpath, value)
-          .isEqualTo(value)
+    xmlProperties.foreach { case (xpath, value) =>
+      val documentSource: InputSource = builtDocumentString
+      assertThat(xpath.evaluate(documentSource, XPathConstants.STRING))
+        .as("Should evaluate XPath %s to %s", xpath, value)
+        .isEqualTo(value)
     }
     // although these cases are working fine the order of attribute is messed up
     assertThat(xmlToString(builtDocument)).is(
@@ -140,12 +139,11 @@ class XmlBuilderTest {
       .unsafeGet
     val builtDocumentString = xmlToString(builtDocument)
 
-    xmlProperties.foreach {
-      case (xpath, value) =>
-        val documentSource: InputSource = builtDocumentString
-        assertThat(xpath.evaluate(documentSource, XPathConstants.STRING))
-          .as("Should evaluate XPath %s to %s", xpath, value)
-          .isEqualTo(value)
+    xmlProperties.foreach { case (xpath, value) =>
+      val documentSource: InputSource = builtDocumentString
+      assertThat(xpath.evaluate(documentSource, XPathConstants.STRING))
+        .as("Should evaluate XPath %s to %s", xpath, value)
+        .isEqualTo(value)
     }
     // although these cases are working fine the order of attribute is messed up
     assertThat(builtDocumentString).is(
@@ -178,12 +176,11 @@ class XmlBuilderTest {
       .unsafeGet
     var builtDocumentString = xmlToString(builtDocument)
 
-    xmlProperties.foreach {
-      case (xpath, value) =>
-        val documentSource: InputSource = builtDocumentString
-        assertThat(xpath.evaluate(documentSource, XPathConstants.STRING))
-          .as("Should evaluate XPath %s to %s", xpath, value)
-          .isEqualTo(value)
+    xmlProperties.foreach { case (xpath, value) =>
+      val documentSource: InputSource = builtDocumentString
+      assertThat(xpath.evaluate(documentSource, XPathConstants.STRING))
+        .as("Should evaluate XPath %s to %s", xpath, value)
+        .isEqualTo(value)
     }
     // although these cases are working fine the order of attribute is messed up
     assertThat(builtDocumentString).is(
@@ -204,12 +201,11 @@ class XmlBuilderTest {
       .unsafeGet
     builtDocumentString = xmlToString(builtDocument)
 
-    xmlProperties.foreach {
-      case (xpath, value) =>
-        val documentSource: InputSource = builtDocumentString
-        assertThat(xpath.evaluate(documentSource, XPathConstants.STRING))
-          .as("Should evaluate XPath %s to %s", xpath, value)
-          .isEqualTo(value)
+    xmlProperties.foreach { case (xpath, value) =>
+      val documentSource: InputSource = builtDocumentString
+      assertThat(xpath.evaluate(documentSource, XPathConstants.STRING))
+        .as("Should evaluate XPath %s to %s", xpath, value)
+        .isEqualTo(value)
     }
     // although these cases are working fine the order of attribute is messed up
     assertThat(builtDocumentString).is(
@@ -257,6 +253,14 @@ class XmlBuilderTest {
     val string        = printer.format(xml).replaceAll(s">\n\\s*(\\w.+?)\n\\s*</", ">$1</") + "\n"
     string.replaceAll("\n", lineSeparator)
   }
+
+  implicit private def toXPathExpression(xpathString: String)(implicit nc: NamespaceContext): XPathExpression = {
+    val xpath = XPathFactory.newInstance().newXPath()
+    Option(nc).foreach(xpath.setNamespaceContext)
+    xpath.compile(xpathString)
+  }
+
+  implicit private def toInputSource(xmlString: String): InputSource = new InputSource(new StringReader(xmlString))
 }
 
 //noinspection ConvertExpressionToSAM
@@ -264,9 +268,10 @@ object XmlBuilderTest {
   implicit private[scala] class JUMapOps[K, V](private val map: java.util.Map[K, V]) extends AnyVal {
     def asScala: Map[K, V] = {
       val linkedHashMap = new mutable.LinkedHashMap[K, V]
-      map.entrySet.forEach { (entry: java.util.Map.Entry[K, V]) =>
+      val iterator      = map.entrySet().iterator()
+      while (iterator.hasNext) {
+        val entry = iterator.next()
         linkedHashMap += entry.getKey -> entry.getValue
-        ()
       }
       linkedHashMap
     }
@@ -280,23 +285,8 @@ object XmlBuilderTest {
     def >>=[LL >: L, RR](f: R => Either[LL, RR]): Either[LL, RR] = either.fold(Left(_), f)
     def unsafeGet(implicit ev: L <:< Throwable): R               = either.fold(ex => throw ev(ex), identity)
   }
-
-  implicit private[scala] def toXPathExpression(
-    xpathString: String
-  )(implicit nc: NamespaceContext = null): XPathExpression = {
-    val xpath = XPathFactory.newInstance().newXPath()
-    Option(nc).foreach(xpath.setNamespaceContext)
-    xpath.compile(xpathString)
-  }
-
-  implicit private[scala] def toInputSource(xmlString: String): InputSource =
-    new InputSource(new StringReader(xmlString))
   implicit private[scala] def asJavaPredicate[A](p: A => Boolean): java.util.function.Predicate[A] =
     new java.util.function.Predicate[A] {
       override def test(a: A): Boolean = p(a)
-    }
-  implicit private[scala] def asJavaConsumer[A](c: A => Unit): java.util.function.Consumer[A] =
-    new java.util.function.Consumer[A] {
-      override def accept(a: A): Unit = c(a)
     }
 }
