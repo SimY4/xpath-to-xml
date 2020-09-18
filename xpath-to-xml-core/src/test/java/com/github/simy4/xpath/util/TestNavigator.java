560b42c09e486c54eed48792628b52100fe384d1
SimY4 <alex.simkin@outlook.com>
2020-09-18 17:24:09 +1000

    Revert JUnit update. Add more mutation coverage.

---
+++ xpath-to-xml-core/src/test/java/com/github/simy4/xpath/util/TestNavigator.java
@@ -0,0 +1,21 @@
+package com.github.simy4.xpath.util;
+
+import com.github.simy4.xpath.spi.Effect;
+import com.github.simy4.xpath.spi.NavigatorSpi;
+
+public class TestNavigator implements NavigatorSpi {
+
+    @Override
+    public boolean canHandle(Object o) {
+        return null != o;
+    }
+
+    @Override
+    public <T> T process(T xml, Iterable<Effect> effects) {
+        if (!canHandle(xml)) {
+            throw new IllegalArgumentException("Argument can't be handled: " + xml);
+        }
+        return xml;
+    }
+
+}
