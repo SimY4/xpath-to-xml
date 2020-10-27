adf1f1e45f4ce2a66cd45ce6af51194cc79ad593
SimY4 <alex.simkin@outlook.com>
2020-10-27 14:07:52 +1100

    rename javax module to jakarta module.

---
+++ xpath-to-json-jakarta/src/test/java/com/github/simy4/xpath/json/navigator/node/JakartaJsonByNameNodeTest.java
@@ -0,0 +1,66 @@
+package com.github.simy4.xpath.json.navigator.node;
+
+import jakarta.json.JsonObject;
+import jakarta.json.spi.JsonProvider;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.entry;
+
+class JakartaJsonByNameNodeTest {
+
+    private static final JsonProvider jsonProvider = JsonProvider.provider();
+
+    private final JsonObject jsonObject = jsonProvider.createObjectBuilder()
+            .add("one", 1)
+            .add("two", 2)
+            .add("three", 3)
+            .build();
+    private final JakartaJsonNode rootNode = new JakartaJsonRootNode(jsonObject);
+    private final JakartaJsonNode byNameNode = new JakartaJsonByNameNode("two", rootNode);
+
+    @Test
+    void shouldRetrieveElementByIndexOnGet() {
+        assertThat(byNameNode.get()).isEqualTo(jsonProvider.createValue(2));
+    }
+
+    @Test
+    void shouldSetElementByIndexOnSet() {
+        byNameNode.set(jsonProvider, jsonProvider.createValue(4));
+
+        assertThat(rootNode.get().asJsonObject()).containsExactly(
+                entry("one", jsonProvider.createValue(1)),
+                entry("two", jsonProvider.createValue(4)),
+                entry("three", jsonProvider.createValue(3))
+        );
+    }
+
+    @Test
+    void shouldRemoveElementByIndexOnSetNull() {
+        byNameNode.set(jsonProvider, null);
+
+        assertThat(rootNode.get().asJsonObject()).containsExactly(
+                entry("one", jsonProvider.createValue(1)),
+                entry("three", jsonProvider.createValue(3))
+        );
+    }
+
+    @Test
+    void shouldTraverseObjectAttributes() {
+        JakartaJsonNode parent = new JakartaJsonRootNode(jsonObject);
+
+        assertThat(parent.attributes()).containsExactlyInAnyOrder(
+                new JakartaJsonByNameNode("one", parent),
+                new JakartaJsonByNameNode("two", parent),
+                new JakartaJsonByNameNode("three", parent)
+        );
+    }
+
+    @Test
+    void shouldTraverseObjectElements() {
+        JakartaJsonNode parent = new JakartaJsonRootNode(jsonObject);
+
+        assertThat(parent.elements()).isEmpty();
+    }
+
+}
---
+++ xpath-to-json-jakarta/src/test/java/com/github/simy4/xpath/json/navigator/node/JakartaJsonRootNodeTest.java
@@ -0,0 +1,44 @@
+package com.github.simy4.xpath.json.navigator.node;
+
+import com.github.simy4.xpath.XmlBuilderException;
+import jakarta.json.JsonArray;
+import jakarta.json.JsonObject;
+import jakarta.json.JsonValue;
+import jakarta.json.spi.JsonProvider;
+import org.junit.jupiter.api.Test;
+
+import javax.xml.namespace.QName;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+class JakartaJsonRootNodeTest {
+
+    private static final JsonProvider jsonProvider = JsonProvider.provider();
+
+    private final JsonObject jsonObject = JsonValue.EMPTY_JSON_OBJECT;
+    private final JakartaJsonNode rootNode = new JakartaJsonRootNode(jsonObject);
+
+    @Test
+    void shouldReturnRootName() {
+        assertThat(rootNode.getName()).isEqualTo(new QName(JakartaJsonNode.DOCUMENT));
+    }
+
+    @Test
+    void shouldReturnRootNode() {
+        assertThat(rootNode.get()).isSameAs(jsonObject);
+    }
+
+    @Test
+    void shouldReplaceRootNodeOnSet() {
+        JsonArray array = JsonValue.EMPTY_JSON_ARRAY;
+        rootNode.set(jsonProvider, array);
+        assertThat(rootNode.get()).isSameAs(array);
+    }
+
+    @Test
+    void shouldThrowOnSetNull() {
+        assertThatThrownBy(() -> rootNode.set(jsonProvider, null)).isInstanceOf(XmlBuilderException.class);
+    }
+
+}
