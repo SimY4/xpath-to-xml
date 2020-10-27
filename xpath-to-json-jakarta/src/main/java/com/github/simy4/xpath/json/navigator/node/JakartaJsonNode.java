c0d0b0d38c6f7534c2c283e9510ad2994dd24e5f
SimY4 <alex.simkin@outlook.com>
2020-10-27 14:07:52 +1100

    rename javax module to jakarta module.

---
+++ xpath-to-json-jakarta/src/main/java/com/github/simy4/xpath/json/navigator/node/JakartaJsonNode.java
@@ -0,0 +1,30 @@
+package com.github.simy4.xpath.json.navigator.node;
+
+import com.github.simy4.xpath.XmlBuilderException;
+import com.github.simy4.xpath.navigator.Node;
+import jakarta.json.JsonValue;
+import jakarta.json.spi.JsonProvider;
+
+import java.util.stream.Stream;
+
+/**
+ * Javax JSON node contract.
+ *
+ * @author Alex Simkin
+ * @since 2.0
+ */
+public interface JakartaJsonNode extends Node {
+
+    JakartaJsonNode getParent();
+
+    void setParent(JakartaJsonNode parent);
+
+    JsonValue get();
+
+    void set(JsonProvider jsonProvider, JsonValue jsonValue) throws XmlBuilderException;
+
+    Stream<JakartaJsonNode> elements();
+
+    Stream<JakartaJsonNode> attributes();
+
+}
