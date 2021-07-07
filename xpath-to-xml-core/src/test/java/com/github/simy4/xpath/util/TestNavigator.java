12ea34269da4354f1d21a646055598672f6eca1f
SimY4 <alex.simkin@outlook.com>
2021-07-07 14:06:36 +1000

    Apache LICENSE headers with inception year.

--- xpath-to-xml-core/src/test/java/com/github/simy4/xpath/util/TestNavigator.java
+++ xpath-to-xml-core/src/test/java/com/github/simy4/xpath/util/TestNavigator.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2021 Alex Simkin
+ * Copyright 2020-2021 Alex Simkin
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
