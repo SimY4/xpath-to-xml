950b9ebf212e8d8e96c3c5f951751b632cb1dfe2
SimY4 <alex.simkin@outlook.com>
2022-03-12 17:07:38 +1100

    Fix license header inception year.

--- xpath-to-xml-core/src/main/java/com/github/simy4/xpath/parser/XPathLexer.java
+++ xpath-to-xml-core/src/main/java/com/github/simy4/xpath/parser/XPathLexer.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2017-2021 Alex Simkin
+ * Copyright 2017-2022 Alex Simkin
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
--- xpath-to-xml-scala/src/main/scala/com/github/simy4/xpath/scala/impl/ToXmlElemOps.scala
+++ xpath-to-xml-scala/src/main/scala/com/github/simy4/xpath/scala/impl/ToXmlElemOps.scala
@@ -1,5 +1,5 @@
 /*
- * Copyright 2018-2021 Alex Simkin
+ * Copyright 2018-2022 Alex Simkin
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
