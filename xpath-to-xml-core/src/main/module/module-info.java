61f86c31458b77540c8ca492b33feb4829d52200
SimY4 <alex.simkin@outlook.com>
2023-04-07 20:59:17 +1000

    Modularize scala modules.

--- xpath-to-xml-core/src/main/module/module-info.java
+++ xpath-to-xml-core/src/main/module/module-info.java
@@ -1,10 +1,10 @@
 module com.github.simych.xpath.core {
-    requires transitive java.xml;
+  requires transitive java.xml;
 
-    exports com.github.simy4.xpath;
-    exports com.github.simy4.xpath.navigator;
-    exports com.github.simy4.xpath.spi;
-    exports com.github.simy4.xpath.util;
+  exports com.github.simy4.xpath;
+  exports com.github.simy4.xpath.navigator;
+  exports com.github.simy4.xpath.spi;
+  exports com.github.simy4.xpath.util;
 
-    uses com.github.simy4.xpath.spi.NavigatorSpi;
+  uses com.github.simy4.xpath.spi.NavigatorSpi;
 }
