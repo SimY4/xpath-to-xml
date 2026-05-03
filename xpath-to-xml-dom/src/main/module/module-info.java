63d72ea3c321fd2e040c986a9ab4536cd76ad5b0
SimY4 <alex.simkin@outlook.com>
2026-05-03 18:04:22 +1000

    Added java.xml as module depenency for dom module.

--- xpath-to-xml-dom/src/main/module/module-info.java
+++ xpath-to-xml-dom/src/main/module/module-info.java
@@ -1,5 +1,6 @@
 module com.github.simych.xpath.dom {
   requires transitive com.github.simych.xpath.core;
+  requires java.xml;
 
   provides com.github.simy4.xpath.spi.NavigatorSpi with
       com.github.simy4.xpath.dom.spi.DomNavigatorSpi;
